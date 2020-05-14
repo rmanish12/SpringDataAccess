fe72e9d6edba076d0c5a016660711b9ec504f6b7
Manish <manish@Manishs-MacBook-Air.local>
2020-05-14 18:29:26 +0530

    spring hibernate java configuration not working

--- src/main/java/com/hibernate/client/Client.java
+++ src/main/java/com/hibernate/client/Client.java
@@ -2,9 +2,11 @@ package com.hibernate.client;
 
 import java.util.List;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.hibernate.config.SpringHibernateConfig;
 import com.hibernate.entity.Employee;
 import com.hibernate.service.EmployeeService;
 
@@ -13,18 +15,19 @@ public class Client {
 	public static void main(String[] args) {
 		
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("hibernate.cfg.xml");
+//		AbstractApplicationContext context = new AnnotationConfigApplicationContext(SpringHibernateConfig.class);
 		
 		EmployeeService empService = context.getBean("employeeService", EmployeeService.class);
 		
 //		Employee emp1 = new Employee(1000, "Alex", "Account");
-//		Employee emp2 = new Employee(1001, "Sam", "HR");
+		Employee emp2 = new Employee(1001, "Sam", "HR");
 		
 //		empService.insert(emp1);
-//		empService.insert(emp2);
+		empService.insert(emp2);
 		
 //		empService.delete(1001);
 		
-		empService.update(1000, "Engineering");
+//		empService.update(1000, "Engineering");
 		
 		List<Employee> allEmployees = empService.getEmployees();
 		for(Employee e: allEmployees) {
---
+++ src/main/java/com/hibernate/config/SpringHibernateConfig.java
@@ -0,0 +1,71 @@
+package com.hibernate.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@ComponentScan({"com.hibernate.service", "com.hibernate.dao"})
+@PropertySource("application.properties")
+public class SpringHibernateConfig {
+
+	@Autowired
+	Environment env;
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(restDataSource());
+		sessionFactory.setPackagesToScan(new String[] {"com.hibernate.entity"});
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		
+		return sessionFactory;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager txnManager = new HibernateTransactionManager();
+		txnManager.setSessionFactory(sessionFactory);
+		
+		return txnManager;
+	}
+	
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+		properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		properties.setProperty("hibernate.globally_quoted_identifiers", "true");
+		
+		return properties;
+	}
+
+	private DataSource restDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		return dataSource;
+	}
+	
+}
